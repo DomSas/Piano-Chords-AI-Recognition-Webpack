d308028a726be2e1657b7dce9e48d29465e04ee2
Dominik Sasko <62658199+DomSas@users.noreply.github.com>
2022-04-13 15:59:08 +0900

    Feat code refactoring (#3)
    * feat: moving functions from PredictingPage.vue to helperFunctions.js to separate logic
    
    * Adding additional build file
    
    Co-authored-by: Dominik Sasko <dominik.sasko@asial.co.jp>

--- src/pages/PredictingPage.vue
+++ src/pages/PredictingPage.vue
@@ -42,6 +42,7 @@
 
 <script>
 import { ref, onMounted } from "vue";
+import helperFunctions from "../js/helperFunctions";
 
 export default {
   setup() {
@@ -49,11 +50,7 @@ export default {
     const currentChordLetter = ref("");
     const chordImageVisible = ref(false);
     const label = ref("waiting...");
-
     let card;
-    // Classifier and model url
-    let classifier;
-    let modelURL = "https://teachablemachine.withgoogle.com/models/oQapXjh1t/";
 
     onMounted(() => {
       randomizeChordLetter();
@@ -73,101 +70,23 @@ export default {
       currentChordLetter.value = chordLetters[Math.floor(Math.random() * 4)];
     };
 
-    const isAndroid = () => {
-      const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-      if (/android/i.test(userAgent)) return true;
-      return false;
-    };
-
-    const isIos = () => {
-      const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-      if (/iPad|iPhone|iPod/i.test(userAgent)) return true;
-      return false;
-    };
-
-    const isMobile = () => {
-      if (window.cordova && (isAndroid() || isIos())) {
-        return true;
-      } else {
-        return false;
-      }
-    };
-
     // Predict piano chords
     const startRecognizingChords = async () => {
-      const options = { probabilityThreshold: 0.7 };
+      helperFunctions.startPrediction((recognizedChord) => {
+        console.log(recognizedChord);
+        label.value = recognizedChord;
 
-      if (isMobile == true) {
-        console.log("here");
-        audioinput.start({
-          streamToWebAudio: true,
-        });
-        audioinput.stop(() => {
-          console.log("stopped");
-        });
-      }
-      // cordova.plugins.diagnostic.requestMicrophoneAuthorization(
-      //   function (status) {
-      //     if (status === cordova.plugins.diagnostic.permissionStatus.GRANTED) {
-      //       console.log("Microphone use is authorized");
-      //     }
-      //   },
-      //   function (error) {
-      //     console.error(error);
-      //   }
-      // );
-
-      // STEP 1: Load the model!
-      classifier = await ml5.soundClassifier(modelURL + "model.json", options);
-      // STEP 2: Start classifying (will listen to mic by default)
-      classifier.classify(gotResults);
-    };
-
-    function gotResults(error, results) {
-      if (error) {
-        console.error(error);
-        return;
-      }
-      // Store the label
-      label.value = results[0].label;
-      console.log(results[0].label);
-
-      if (results[0].label === currentChordLetter.value) {
-        card.classList.toggle("is-green");
-        setTimeout(() => {
+        if (recognizedChord === currentChordLetter.value) {
           card.classList.toggle("is-green");
-          randomizeChordLetter();
-        }, 1500);
-      }
-    }
-
-    const getAudioCordovaPluginMedia = () => {
-      let src = "myrecording.mp3";
-      var mediaRec = new Media(
-        src,
-        // success callback
-        function () {
-          console.log("recordAudio():Audio Success");
-        },
-
-        // error callback
-        function (err) {
-          console.log("recordAudio():Audio Error: " + err.code);
+          setTimeout(() => {
+            card.classList.toggle("is-green");
+            randomizeChordLetter();
+          }, 1500);
         }
-      );
-
-      // Record audio
-      mediaRec.startRecord();
-
-      // Stop recording after 10 seconds
-      setTimeout(function () {
-        mediaRec.stopRecord();
-        mediaRec.play();
-      }, 5000);
+      });
     };
 
     return {
-      getAudioCordovaPluginMedia,
       startRecognizingChords,
       flipCard,
       randomizeChordLetter,
